27d13b4ef5e8f2ad70d8579063efcb3cb1b37329
jesus cantor <jedacan58@gmail.com>
2025-09-30 12:32:39 -0500

    Edit associations.js by fixed test file

--- models/associations.js
+++ models/associations.js
@@ -2,7 +2,9 @@ const Parking = require('./Parking');
 const Vehicle = require('./Vehicle');
 const User = require('./User');
 
+User.hasMany(Parking, { foreignKey: 'id_partner', as: 'Parkings' });
 Parking.belongsTo(User, { foreignKey: 'id_partner', as: 'partner' });
+
 Parking.hasMany(Vehicle, { as: 'vehicles', foreignKey: 'id_parking' });
 Vehicle.belongsTo(Parking, { foreignKey: 'id_parking' });
 
