c4c669bd50678cd448b844673e72ba2807a1102d
jesus cantor <jedacan58@gmail.com>
2025-09-30 17:55:47 -0500

    Edit route updateUser

--- middlewares/validateUser.js
+++ middlewares/validateUser.js
@@ -9,10 +9,21 @@ const validateUserNew = [
     validate
 ];
 
+const validateUserUpdated = [
+    check('id', 'El ID parking es obligatorio').isInt({min: 1}),
+    check('username','El username es obligatorio').not().isEmpty(),
+    check('password','El password es obligatorio').not().isEmpty(),
+    check('email','El email es obligatorio').isEmail(),
+    check('role', 'El rol es inválido').optional().isIn(['SOCIO', 'ADMIN']),
+    validate
+];
+
+
+
 const validateUserLogin = [
     check('email','El email es obligatorio').isEmail(),
     check('password','El password es obligatorio').not().isEmpty(),
     validate
 ];
 
-module.exports = { validateUserNew, validateUserLogin };
+module.exports = { validateUserNew, validateUserLogin, validateUserUpdated };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,12 @@
 const { createUser, getAllUsers, updateUser, getAllUsersWithAdmin } = require('../controllers/userController');
 const { Router } = require('express');
-const { validateUserNew } = require('../middlewares/validateUser');
+const { validateUserNew, validateUserUpdated } = require('../middlewares/validateUser');
 const { validateJWT } = require('../middlewares/validate-jwt');
 const router = Router();
 
 router.post('/', validateJWT, ...validateUserNew, createUser);
 
-router.put('/', validateJWT, ...validateUserNew, updateUser);
+router.put('/:id', validateJWT, ...validateUserUpdated, updateUser);
 
 router.get('/', validateJWT, getAllUsers);
 
